dc3a77b0fee495f6370911ede749835c4b10deb4
TOPTCOPEXOM <tcurpal@gmail.com>
2022-09-07 16:23:14 +0200

    prices

--- src/js/files/current-link.js
+++ src/js/files/current-link.js
@@ -1,1 +1,17 @@
-document.querySelectorAll('a href="'+document.URL+'"]').forEach(function(elem){elem.className += ' current-link'});
+
+    function highlightCurrent() {
+			const curPage = document.URL;
+			const links = document.getElementsByTagName('a');
+			for (let link of links) {
+				if (link.href == curPage) {
+					link.classList.add("current");
+				}
+			}
+		}
+
+
+		document.onreadystatechange = () => {
+			if (document.readyState === 'complete') {
+				highlightCurrent()
+			}
+		};
