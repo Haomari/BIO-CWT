7e163e9221736e49e2081a85b667075339bc49be
TOPTCOPEXOM <tcurpal@gmail.com>
2022-09-14 15:19:10 +0200

    parallax

--- src/js/files/scroll/simplebar.js
+++ src/js/files/scroll/simplebar.js
@@ -6,12 +6,20 @@ import 'simplebar/dist/simplebar.css';
 // Добавляем к блоку атрибут data-simplebar
 
 // Также можно инициализировать следующим кодом, применяя настройки
-/*
-if (document.querySelectorAll('[data-simplebar]').length) {
-	document.querySelectorAll('[data-simplebar]').forEach(scrollBlock => {
+
+/* if (document.getElementById('scroll').length) {
+	document.getElementById('scroll').forEach(scrollBlock => {
 		new SimpleBar(scrollBlock, {
-			autoHide: false
+			autoHide: false,
+			forceVisible: true|'x'|'y' ,
+			timeout: 1000,
+			scrollbarMinSize: 1100,
 		});
 	});
-}
-*/
+} */
+
+new SimpleBar(document.getElementById('scroll'), {
+	autoHide: false,
+	timeout: 1000,
+	scrollbarMinSize: 100,
+});
