66dbf8adb7014b88eae26f3efc29984b5039f6a5
TOPTCOPEXOM <tcurpal@gmail.com>
2022-09-16 20:16:07 +0200

    page (404 + script)

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,9 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+let n = location.href;
+
+if(n.includes(".html")) {
+	location = "404"
+}
