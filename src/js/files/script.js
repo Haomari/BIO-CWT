b5a350fab0148c129306a70b1547e0dd75539902
TOPTCOPEXOM <tcurpal@gmail.com>
2022-10-11 18:53:39 +0200

    v1.0

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,7 +6,7 @@ import { flsModules } from "./modules.js";
 let href = location.href;
 let hrefChek = (href.includes("home.html"))||(href.includes("gallery.html"))||(href.includes("prices.html"))||(href.includes("about-us.html"))||(href.includes("contact.html"));
 
-if(href.includes(".htmo")) {
+if(href.includes(".html")) {
 	location = "404"
 }else if (!hrefChek) {
 	location = "404"
