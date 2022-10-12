6d21ec48300ff3f964a3b4568557a906d6954c80
TOPTCOPEXOM <tcurpal@gmail.com>
2022-10-12 16:24:04 +0200

    v1.1

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,11 +6,11 @@ import { flsModules } from "./modules.js";
 let href = location.href;
 let hrefChek = (href.includes("home.html"))||(href.includes("gallery.html"))||(href.includes("prices.html"))||(href.includes("about-us.html"))||(href.includes("contact.html"));
 
-if(href.includes(".html")) {
-	location = "404"
-}else if (!hrefChek) {
-	location = "404"
-} 
+// if(href.includes(".html")) {
+// 	location = "404"
+// }else if (!hrefChek) {
+// 	location = "404"
+// } 
 
 
 /* if (window.innerWidth >= 767) {
